083267e3fe8f2515abadb58e7d39b8bfe820fcde
BIT Student <classroom@bgit.rs>
2018-03-05 18:13:49 +0100

    jQUERY

---
+++ jQUERY/main.js
@@ -0,0 +1,13 @@
+$(function () {
+    var el = $('li:first');
+    var allEl = $('li');
+    var elAct = $('li.active');
+    var middle = $("li:eq(2)")
+
+    el.addClass('bottomBorder');
+    allEl.addClass('toUpperCase');
+    elAct.addClass('fontColor');
+    middle.addClass('changeBackground');
+
+
+})
